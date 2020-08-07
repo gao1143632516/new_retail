3cb1468f5e8e4b5a91f395079845da607bc3f6ec
小强 <1143632516@qq.com>
2020-08-07 14:35:00 +0800

    Add files via upload

---
+++ config/dev.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    NODE_ENV: '"development"',
+     BASE_API: '"http://gmq.company.e9w.me/api"',
+    //BASE_API:   '"http://192.168.19.52:8888/api"',
+    APP_ORIGIN: '"https://wallstreetcn.com"'
+}
---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+    NODE_ENV: '"production"',
+    //BASE_API: '"http://xy.skywalker.19where.com/api"',
+    BASE_API: '"http://retail.company.e9w.me/api"',
+    APP_ORIGIN: '"https://wallstreetcn.com"'
+};
