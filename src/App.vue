8eee32e47a8776c6391aafc29c4676e405a19a56
小强 <1143632516@qq.com>
2020-08-07 15:27:03 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+  export default{
+    name: 'APP'
+  }
+</script>
+
+<style lang="scss">
+   @import "../static/css/main.css";
+   @import "../static/css/color-dark.css";     /*深色主题*/
+  @import './styles/index.scss'; // 全局自定义的css样式
+</style>
